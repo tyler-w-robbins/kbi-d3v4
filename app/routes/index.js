b3c37760c736d6faae0cd6c78b5aff15571384a3
Grace <Grace@Gracefulco.de>
2016-09-08 11:12:36 -0400

    Implemented autocomplete suggestions

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'KBI v1.0' });
+  res.render('index', { title: 'Knowledge Base Interface' });
 });
 
 module.exports = router;
